4f14b5447575eb46db58e74efec65d61e9cf06bd
sunhao_global <sunhao@guigu.com>
2017-08-02 15:50:21 +0800

    第一次

---
+++ src/ServletTest/UserServlet.java
@@ -0,0 +1,58 @@
+package ServletTest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter writer = response.getWriter();
+		
+		String existingEmail = "tom@163.com";
+		
+		String userEmail = request.getParameter("userEmail");
+		
+		System.out.println(userEmail);
+		
+		if(existingEmail.equals(userEmail)) {
+			
+			writer.write("false");
+			
+		}else{
+			
+			writer.write("true");
+			
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request,response);
+	}
+
+}
