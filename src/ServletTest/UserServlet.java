d549fd0e8cda064d5ef2b3fce351df75bf23ce05
sunhao_global <sunhao@guigu.com>
2017-08-02 15:53:49 +0800

    2

--- src/ServletTest/UserServlet.java
+++ src/ServletTest/UserServlet.java
@@ -14,18 +14,12 @@ import javax.servlet.http.HttpServletResponse;
 public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+  
     public UserServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		PrintWriter writer = response.getWriter();
 		
@@ -47,9 +41,6 @@ public class UserServlet extends HttpServlet {
 		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		doGet(request,response);
